0f0b0bc183e83e0e7d875abe4f1762360db42849
Rodrigo Gabriel <rgabriel15@gmail.com>
2018-06-04 19:55:09 -0300

    Update ExecuteControllerFunction.js

--- ExecuteControllerFunction.js
+++ ExecuteControllerFunction.js
@@ -1,44 +1,46 @@
 <script type="text/javascript">
-	function ExecuteControllerFunction(controllerClassName, controllerFunctionName, inputData, inputDataType, onSuccessFunction, onErrorFunction, isPostFunction) {
-		if (typeof controllerClassName != "string" || controllerClassName.length < 1)
+    function ExecuteControllerFunction(controllerClassName, controllerFunctionName, inputData, inputDataType, onSuccessFunction, onErrorFunction, isPostFunction, isFile) {
+        if (typeof controllerClassName != "string" || controllerClassName.length < 1)
             throw "ArgumentException: controllerClassName.";
-		if (typeof controllerFunctionName != "string" || controllerFunctionName.length < 1)
+        if (typeof controllerFunctionName != "string" || controllerFunctionName.length < 1)
             throw "ArgumentException: controllerFunctionName.";
 
-		if (inputDataType != null) {
-			var inputDataTypeErrorMessage = " inputDataType must be \"xml\", \"json\", \"script\", \"html\" or null."
-			if (typeof inputDataType != "string") {
-				throw "ArgumentException: inputDataType." + inputDataTypeErrorMessage;
-			} else {
-				inputDataType = inputDataType.toLowerCase();
-				if (inputDataType != "xml"
+        if (inputDataType != null) {
+            var inputDataTypeErrorMessage = " inputDataType must be \"xml\", \"json\", \"script\", \"html\" or null."
+            if (typeof inputDataType != "string") {
+                throw "ArgumentException: inputDataType." + inputDataTypeErrorMessage;
+            } else {
+                inputDataType = inputDataType.toLowerCase();
+                if (inputDataType != "xml"
 					&& inputDataType != "json"
 					&& inputDataType != "script"
 					&& inputDataType != "html") {
-					throw "ArgumentException: inputDataType." + inputDataTypeErrorMessage;
-				}
-			}
-		}
+                    throw "ArgumentException: inputDataType." + inputDataTypeErrorMessage;
+                }
+            }
+        }
 
-		if (typeof onSuccessFunction != "function")
-			if (onSuccessFunction != null) {
-				throw "ArgumentException: onSuccessFunction.";
-			} else {
-				onSuccessFunction = function (data) {
-					alert(data);
-				}
-			}			
-			
-		if (typeof onErrorFunction != "function")
-			if (onErrorFunction != null) {
-				throw "ArgumentException: onErrorFunction.";
-			} else {
-				onErrorFunction = function (xhr, ajaxOptions, thrownError) {
-					alert(thrownError.toString());
-				}
-			}
-		
+        if (typeof onSuccessFunction != "function")
+            if (onSuccessFunction != null) {
+                throw "ArgumentException: onSuccessFunction.";
+            } else {
+                onSuccessFunction = function (data) {
+                    alert(data);
+                }
+            }
+
+        if (typeof onErrorFunction != "function")
+            if (onErrorFunction != null) {
+                throw "ArgumentException: onErrorFunction.";
+            } else {
+                onErrorFunction = function (xhr, ajaxOptions, thrownError) {
+                    alert(thrownError.toString());
+                }
+            }
+
         isPostFunction = typeof isPostFunction == "boolean" ? isPostFunction : true;
+        isFile = typeof isPostFunction == "boolean" ? isPostFunction : false;
+
         var functionType = isPostFunction ? "POST" : "GET";
         var ajaxSettings = {
             url: "/" + controllerClassName + "/" + controllerFunctionName,
@@ -55,12 +57,13 @@
             error: onErrorFunction
         }
 
-        if (inputDataType == null) {
+        if (isFile)
+        {
             ajaxSettings.cache = false;
             ajaxSettings.contentType = false;
             ajaxSettings.processData = false;
         }
 
         $.ajax(ajaxSettings);
-	}
+    }
 </script>
